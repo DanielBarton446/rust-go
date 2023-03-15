e1952e9b6e4869817f9fbca808d2d2d1e201a500
Dalton Maahs <dalton.maahs@yourbind.com>
2023-03-14 23:18:33 -0500

    update integrations, add units, refactor parse_move_position

--- go/src/ui.rs
+++ go/src/ui.rs
@@ -22,20 +22,77 @@ where
 }
 
 fn parse_move_position(mv: String) -> Result<(usize, usize)> {
-    let parts = mv.split_at(1);
+    if mv.len() != 2 {
+        bail!("Move should be 2 characters");
+    }
+    let row = mv.as_bytes()[0].to_ascii_uppercase();
+    let col = mv.as_bytes()[1];
 
     // Error if not alphabetic
-    if !parts.0.chars().all(char::is_alphabetic) {
-        bail!("Non-alphabetical first coordinate");
+    if !row.is_ascii_alphabetic() {
+        bail!("Non-alphabetical row");
     }
-
-    // Error if not usize
-    if !parts.1.chars().all(char::is_numeric) {
-        bail!("Non-digit second coordinate");
+    if !col.is_ascii_digit() {
+        bail!("Non-digit column");
     }
 
     // shouldn't unwrap here
-    let row = parts.0.chars().next().with_context(|| "Input was empty")? as usize - 'A' as usize;
-    let col: usize = parts.1.parse().with_context(|| "Input was only 1 character")?;
-    Ok((row, col - 1))
+    let row = (row - b'A') as usize;
+    let col: usize = (col - b'0') as usize - 1;
+    Ok((row, col))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_move_works_uppper_case() {
+        let input = String::from("A1");
+        let res = parse_move_position(input).unwrap();
+        assert_eq!((0, 0), res);
+    }
+
+    #[test]
+    fn parse_move_works_lower_case() {
+        let input = String::from("a1");
+        let res = parse_move_position(input).unwrap();
+        assert_eq!((0, 0), res);
+    }
+
+    #[test]
+    fn parse_move_solo_alpha_should_error() {
+        let input = String::from("a");
+        parse_move_position(input).unwrap_err();
+    }
+
+    #[test]
+    fn parse_move_solo_digit_should_error() {
+        let input = String::from("1");
+        parse_move_position(input).unwrap_err();
+    }
+
+    #[test]
+    fn parse_move_empty_should_error() {
+        let input = String::from("");
+        parse_move_position(input).unwrap_err();
+    }
+
+    #[test]
+    fn parse_move_all_digits_should_error() {
+        let input = String::from("11");
+        parse_move_position(input).unwrap_err();
+    }
+
+    #[test]
+    fn parse_move_all_alpha_should_error() {
+        let input = String::from("aa");
+        parse_move_position(input).unwrap_err();
+    }
+
+    #[test]
+    fn parse_move_extra_char_should_error() {
+        let input = String::from("a1b");
+        parse_move_position(input).unwrap_err();
+    }
 }
